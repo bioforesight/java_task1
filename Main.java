354b19234c0e284257090fad470f5d2e32599385
bioforesight <144956903+bioforesight@users.noreply.github.com>
2025-02-27 10:17:57 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Как тебя зовут?");
+        String name = in.next();
+        System.out.printf("Привет, %s!", name);
+    }
+}
